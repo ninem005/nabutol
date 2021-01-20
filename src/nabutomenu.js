effd19a44ec9f131231bb5e28d0ba24bbd63e22f
ninem005 <76542279+ninem005@users.noreply.github.com>
2021-01-19 22:55:02 -0400

    Add files via upload

---
+++ src/nabutomenu.js
@@ -0,0 +1,12 @@
+const nabutomenu = (prefix, pushname) => {
+    return `◪ *Comandos do bruxinho*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.nabutomenu = nabuto
